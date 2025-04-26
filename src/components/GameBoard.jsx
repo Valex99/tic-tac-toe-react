b84ae57f2a5e65db397a72591640698c6ca07559
valex <val.matej99@gmail.com>
2025-04-26 15:34:34 +0200

    Clicking buttons work

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
+export default function GameBoard() {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  // Function that triggers when button is clicked
+  function handleSelectSquare(rowIndex, colIndex) {
+    // State is Immutable!
+    setGameBoard((prevGameBoard) => {
+      const updatedBoard = [
+        ...prevGameBoard.map((innerArray) => [...innerArray]),
+      ];
+      updatedBoard[rowIndex][colIndex] = "X";
+      return updatedBoard;
+    });
+  }
+
+  return (
+    // We use map to go through each row -> for each row we want to have list item;
+    // Now inside ol we again want to output something dynamically -> we map through row
+    <ol id="game-board">
+      {gameBoard.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
+                  {playerSymbol}
+                </button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
+}
